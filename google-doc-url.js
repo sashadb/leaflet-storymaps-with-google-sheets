9ffda67622c39f1e14376ff84130369e6dc23225
sashadb <sashadbrown@gmail.com>
2019-03-14 14:26:20 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTNFEZkqwmw6Ci4PJB7cvIrHHxZ4N0oOqkTyt9MzWj7qGpjBFl_yn5wsyOhsQ8QU0hGsnuYu4n0ahxM/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/10QqNuAKt3XprJUgfbmCd2X5scn44sZy6XpZFgkTggqw/edit#gid=0';
