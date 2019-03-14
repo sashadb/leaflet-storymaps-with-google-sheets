5fda7bb015455adb9f51c4fc76ae3837311159b6
sashadb <sashadbrown@gmail.com>
2019-03-14 14:29:19 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/10QqNuAKt3XprJUgfbmCd2X5scn44sZy6XpZFgkTggqw/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/10QqNuAKt3XprJUgfbmCd2X5scn44sZy6XpZFgkTggqw/edit?usp=sharing';
