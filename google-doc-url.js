8c84e106803027f11df494b1c2156b84148b84df
sashadb <sashadbrown@gmail.com>
2019-03-14 14:13:44 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTNFEZkqwmw6Ci4PJB7cvIrHHxZ4N0oOqkTyt9MzWj7qGpjBFl_yn5wsyOhsQ8QU0hGsnuYu4n0ahxM/pubhtml';
